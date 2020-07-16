59d0e0235244c77fcf78de50f05e685f7b4aedc4
Minwoo Jeong <mwjeong.sci@gmail.com>
2020-07-16 16:42:36 +0900

    Support forced overwriting for downloaded data

--- src/download.py
+++ src/download.py
@@ -2,6 +2,7 @@
 """
 Script to download essential data for category-wide association study (CWAS)
 """
+import argparse
 import os
 
 import yaml
@@ -19,6 +20,12 @@ def main():
     filepath_conf_path = os.path.join(project_dir, 'conf', 'download_filepaths.yaml')
     fileurl_conf_path = os.path.join(project_dir, 'conf', 'download_fileurls.yaml')
 
+    # Parse arguments
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument('-f', '--force_overwrite', dest='force_overwrite', action='store_const',
+                        const=1, default=0, help='Force to overwrite when downloading data (Default: 0)')
+    args = parser.parse_args()
+
     # Parse the configuration files
     with open(filepath_conf_path) as filepath_conf_file:
         filepath_dict = yaml.safe_load(filepath_conf_file)
@@ -31,11 +38,11 @@ def main():
         fileurl_dict = yaml.safe_load(fileurl_conf_file)
 
     # Download the data
-    download_data(filepath_dict, fileurl_dict)
+    download_data(filepath_dict, fileurl_dict, args.force_overwrite)
     print(f'[{get_curr_time()}, Progress] Done')
 
 
-def download_data(filepath_dict: dict, fileurl_dict: dict):
+def download_data(filepath_dict: dict, fileurl_dict: dict, force_overwrite: int):
     """ Download essential data using wget commands"""
 
     for file_group in fileurl_dict:
@@ -46,7 +53,7 @@ def download_data(filepath_dict: dict, fileurl_dict: dict):
         for data_key in fileurl_dict[file_group]:
             data_dest_path = filepath_dict[file_group][data_key]
 
-            if os.path.isfile(data_dest_path):
+            if not force_overwrite and os.path.isfile(data_dest_path):
                 print(f'[{get_curr_time()}, INFO] "{data_dest_path}" already exists. Skip downloading this file.')
             else:
                 cmd = f'wget -O {data_dest_path} {fileurl_dict[file_group][data_key]}'
