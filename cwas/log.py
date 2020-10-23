9288cf59df1f40d77e28a3d11ae1949a466eb7d1
Minwoo Jeong <mwjeong.sci@gmail.com>
2020-10-24 00:48:53 +0900

    Add a function '_eprint'

--- cwas/log.py
+++ cwas/log.py
@@ -6,19 +6,28 @@ from datetime import datetime
 
 
 def print_arg(arg_name: str, arg_val: str):
-    print(f'[ARG] {arg_name}: {arg_val}', file=sys.stderr)
+    _eprint(f'[ARG] {arg_name}: {arg_val}')
 
 
 def print_progress(msg: str):
-    print(f'[{_get_curr_time()}, Progress] {msg}', file=sys.stderr)
+    _eprint(f'[{_get_curr_time()}, Progress] {msg}')
 
 
 def print_warn(msg: str):
-    print(f'[{_get_curr_time()}, WARNING] {msg}', file=sys.stderr)
+    _eprint(f'[{_get_curr_time()}, WARNING] {msg}')
 
 
 def print_err(msg: str):
-    print(f'[{_get_curr_time()}, ERROR] {msg}', file=sys.stderr)
+    _eprint(f'[{_get_curr_time()}, ERROR] {msg}')
+
+
+# Ref:
+# https://stackoverflow.com/questions/5574702/how-to-print-to-stderr-in-python
+def _eprint(*args, **kwargs):
+    """
+    print to the stderr
+    """
+    print(*args, file=sys.stderr, **kwargs)
 
 
 def _get_curr_time() -> str:
