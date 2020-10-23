9b21e6225f778a75298c173381334095cd7acaf3
Minwoo Jeong <mwjeong.sci@gmail.com>
2020-10-24 00:44:42 +0900

    Add a function 'print_arg'

--- cwas/log.py
+++ cwas/log.py
@@ -5,8 +5,12 @@ import sys
 from datetime import datetime
 
 
+def print_arg(arg_name: str, arg_val: str):
+    print(f'[ARG] {arg_name}: {arg_val}', file=sys.stderr)
+
+
 def print_progress(msg: str):
-    print(f'[{_get_curr_time()}, Progress] {msg}')
+    print(f'[{_get_curr_time()}, Progress] {msg}', file=sys.stderr)
 
 
 def print_warn(msg: str):
