71d4f46c1cac564d17fea61264dbeab08587e9a6
Minwoo Jeong <mwjeong.sci@gmail.com>
2020-12-24 09:40:13 +0900

    Create an interface for 'Categorization' class

---
+++ cwas/categorization.py
@@ -0,0 +1,20 @@
+import argparse
+
+from runnable import Runnable
+
+
+class Categorization(Runnable):
+    @staticmethod
+    def _create_arg_parser() -> argparse.ArgumentParser:
+        pass
+
+    @staticmethod
+    def _print_args(args: argparse.Namespace):
+        pass
+
+    @staticmethod
+    def _check_args_validity(args: argparse.Namespace):
+        pass
+
+    def run(self):
+        pass
