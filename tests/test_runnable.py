0547b1f8dd0bdfee682c07864945eb52166b09d1
Minwoo Jeong <mwjeong.sci@gmail.com>
2021-01-03 23:44:57 +0900

    Make 'tests' directory and create 'test_runnable.py'

---
+++ tests/test_runnable.py
@@ -0,0 +1,21 @@
+import argparse
+import unittest
+from unittest.mock import patch
+
+from cwas.runnable import Runnable
+
+
+class TestRunnable(unittest.TestCase):
+    # Enable the Runnable class to be instantiated
+    # by assign an empty set to Runnable.__abstractmethods__
+    @patch.multiple(Runnable, __abstractmethods__=set())
+    def test_get_instance(self):
+        inst = Runnable.get_instance()
+        assert hasattr(inst, 'args')
+        assert isinstance(inst.args, argparse.Namespace)
+
+    @patch.multiple(Runnable, __abstractmethods__=set())
+    def test_run(self):
+        inst = Runnable.get_instance()
+        inst.run()
+        assert 1
