bda6fb5622b2f49ca3d244437aa905f037a7ce7e
Minwoo Jeong <mwjeong.sci@gmail.com>
2021-01-05 22:59:46 +0900

    Use an empty list as an argument when calling the Runnable.get_instance method

--- tests/test_runnable.py
+++ tests/test_runnable.py
@@ -10,12 +10,12 @@ class TestRunnable(unittest.TestCase):
     # by assign an empty set to Runnable.__abstractmethods__
     @patch.multiple(Runnable, __abstractmethods__=set())
     def test_get_instance(self):
-        inst = Runnable.get_instance()
+        inst = Runnable.get_instance(argv=list())
         assert hasattr(inst, 'args')
         assert isinstance(inst.args, argparse.Namespace)
 
     @patch.multiple(Runnable, __abstractmethods__=set())
     def test_run(self):
-        inst = Runnable.get_instance()
+        inst = Runnable.get_instance(argv=list())
         inst.run()
         assert 1
