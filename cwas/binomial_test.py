8d5ac46325819d5679b413e11320b68894847de5
Minwoo Jeong <mwjeong.sci@gmail.com>
2021-01-10 22:34:51 +0900

    Create interfaces for two burden tests: Binomial and permutation tests

---
+++ cwas/binomial_test.py
@@ -0,0 +1,20 @@
+import argparse
+
+from burden_test import BurdenTest
+
+
+class BinomialTest(BurdenTest):
+    @staticmethod
+    def _create_arg_parser() -> argparse.ArgumentParser:
+        pass
+
+    @staticmethod
+    def _print_args(args: argparse.Namespace):
+        pass
+
+    @staticmethod
+    def _check_args_validity(args: argparse.Namespace):
+        pass
+
+    def test(self):
+        pass
