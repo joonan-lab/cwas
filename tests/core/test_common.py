88294e216c3d62ef5fd4dd6ffb85a44016c428bc
Minwoo Jeong <mwjeong.sci@gmail.com>
2021-02-04 23:15:46 +0900

    Add 'test_div_dist_num' function

--- tests/core/test_common.py
+++ tests/core/test_common.py
@@ -2,6 +2,7 @@
 Test the methods in cwas.core.common
 """
 import numpy as np
+import pytest
 
 import cwas.core.common as common
 
@@ -24,3 +25,26 @@ def test_cmp_two_arr():
     # Arrays with the same items but not in the same order
     np.random.shuffle(arr2)
     assert common.cmp_two_arr(arr1, arr2)
+
+
+def test_div_dist_num():
+    # Raise ValueError if one of any argument is not a positive integer.
+    with pytest.raises(ValueError):
+        _ = common.div_dist_num(-1, 5)
+
+    with pytest.raises(ValueError):
+        _ = common.div_dist_num(5, -1)
+
+    # Raise ValueError if 'num' is less than 'num_group'.
+    with pytest.raises(ValueError):
+        _ = common.div_dist_num(4, 5)
+
+    num = 5
+    num_group = 5
+    expected = [1, 1, 1, 1, 1]
+    assert expected == common.div_dist_num(num, num_group)
+
+    for i in range(5):
+        num += 1
+        expected[i] += 1
+        assert expected == common.div_dist_num(num, num_group)
