4e00410f86558caf52e03bbcf0a6e39aa3aa04a1
Minwoo Jeong <mwjeong.sci@gmail.com>
2021-02-04 22:45:46 +0900

    Correct the conditions to raise the ValueErrors

--- cwas/core/common.py
+++ cwas/core/common.py
@@ -22,7 +22,7 @@ def cmp_two_arr(array1: np.ndarray, array2: np.ndarray) -> bool:
 
 def div_dist_num(num: int, num_group: int) -> list:
     """ Divide and distribute the number to each group almost equally. """
-    if num >= num_group:
+    if num < num_group:
         raise ValueError(
             'The argument "num" must be larger than the argument "num_group".'
         )
@@ -43,7 +43,7 @@ def div_dist_num(num: int, num_group: int) -> list:
 
 def div_list(in_list: list, num_sub_list: int) -> list:
     """ Divide the input list into multiple sub-lists """
-    if len(in_list) >= num_sub_list:
+    if len(in_list) < num_sub_list:
         raise ValueError(
             'The length of the input list must be larger '
             'than the argument "num_sub_list".'
