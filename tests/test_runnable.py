b3330144a812dda23f2f413045fc959a142b0aa2
Minwoo Jeong <mwjeong.sci@gmail.com>
2021-03-02 22:48:34 +0900

    Add 'test__assign_config_to_attr' function

--- tests/test_runnable.py
+++ tests/test_runnable.py
@@ -1,5 +1,7 @@
 import argparse
+import pathlib
 import unittest
+import pytest
 from unittest.mock import patch
 
 from cwas.runnable import Runnable
@@ -19,3 +21,14 @@ class TestRunnable(unittest.TestCase):
         inst = Runnable.get_instance(argv=list())
         inst.run()
         assert 1
+
+    @patch.multiple(Runnable, __abstractmethods__=set())
+    def test__assign_config_to_attr(self):
+        inst = Runnable.get_instance(argv=list())
+        inst._assign_config_to_attr('gene_mat_path', 'gene_matrix.txt')
+        assert hasattr(inst, 'gene_mat_path')
+        assert isinstance(getattr(inst, 'gene_mat_path'), pathlib.Path)
+
+        # Set a fake file
+        with pytest.raises(FileNotFoundError):
+            inst._assign_config_to_attr('non_exist', 'non_exist.txt')
