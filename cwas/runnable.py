26ad743b40f850b5c59ac53ca60c64b12dbe7893
Minwoo Jeong <mwjeong.sci@gmail.com>
2021-03-02 22:34:05 +0900

    Add '_assign_config_to_attr' method

--- cwas/runnable.py
+++ cwas/runnable.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 import argparse
 from abc import ABC, abstractmethod
+from importlib.resources import path
 
 from cwas.utils.log import print_err
 
@@ -9,11 +10,7 @@ from cwas.utils.log import print_err
 class Runnable(ABC):
     def __init__(self, args: argparse.Namespace):
         self.args = args
-        try:
-            self._set_attr()
-        except FileNotFoundError:
-            print_err('One of configuration files or resources does not exist.')
-            raise
+        self._set_attr()
 
     @abstractmethod
     def _set_attr(self):
@@ -22,7 +19,19 @@ class Runnable(ABC):
         """
         pass
 
-
+    def _assign_config_to_attr(self, attr_name: str, config_filename: str):
+        """
+        Assign the path of a configuration file in cwas.config as an attribute
+        of the instance of this class. The type of the attribute is
+        pathlib.Path.
+        """
+        try:
+            with path('cwas.config', config_filename) as p:
+                setattr(self, attr_name, p)
+        except FileNotFoundError:
+            print_err(f'The configuration file \'{config_filename}\''
+                      f'does not exist in cwas.config.')
+            raise
 
     @classmethod
     def get_instance(cls, argv: list = None) -> Runnable:
