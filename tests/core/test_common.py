4fcb7b7318f92a40dc9c09d867b98abdc3ad0d3c
Minwoo Jeong <mwjeong.sci@gmail.com>
2021-03-12 17:56:07 +0900

    Add 'test_int_to_bit_arr' function

--- tests/core/test_common.py
+++ tests/core/test_common.py
@@ -128,3 +128,21 @@ def test_swap_label():
 
     with pytest.raises(AssertionError) as e_info:
         assert group_id in str(e_info.value)
+
+def test_int_to_bit_arr():
+    # A negative integer is not allowed as an argument.
+    with pytest.raises(ValueError):
+        _ = common.int_to_bit_arr(-1, 5)
+
+    with pytest.raises(ValueError):
+        _ = common.int_to_bit_arr(5, -1)
+
+    assert common.int_to_bit_arr(5, 0).size == 0
+    assert (common.int_to_bit_arr(1, 3) == np.array([0, 0, 1])).all()
+    assert (common.int_to_bit_arr(2, 3) == np.array([0, 1, 0])).all()
+    assert (common.int_to_bit_arr(3, 3) == np.array([0, 1, 1])).all()
+    assert (common.int_to_bit_arr(4, 3) == np.array([1, 0, 0])).all()
+    assert (common.int_to_bit_arr(5, 3) == np.array([1, 0, 1])).all()
+    assert (common.int_to_bit_arr(6, 3) == np.array([1, 1, 0])).all()
+    assert (common.int_to_bit_arr(7, 3) == np.array([1, 1, 1])).all()
+
