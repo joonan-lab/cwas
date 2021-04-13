8fc0c311bf24f51eb0aff6dad3970f9d804cd8c4
Minwoo Jeong <mwjeong.sci@gmail.com>
2021-04-13 22:31:05 +0900

    Print logs when it starts to execute

--- cwas/cli.py
+++ cwas/cli.py
@@ -1,16 +1,19 @@
 """
-Command line tool for CWAS
+Command line tool for Category-wide association study (CWAS)
 """
 import sys
 
 from cwas.factory import get_runnable
+from cwas.utils.log import print_log
 
 
 def main():
     # TODO: Add conditional statements or exception handling. This is very
     #  error-prone.
+    print_log('LOG', 'Category-Wide Association Study (CWAS)')
     cwas_step_name = sys.argv[1]
     cwas_args = sys.argv[2:]
     cwas_obj = get_runnable(cwas_step_name)
+    print_log('LOG', f'Current step: {cwas_obj.__name__}')
     cwas_inst = cwas_obj.get_instance(cwas_args)
     cwas_inst.run()
