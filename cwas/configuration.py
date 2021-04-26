ad82fbc4ac2355ed765d3d4fcc74d2a47930ef4b
Minwoo Jeong <mwjeong.sci@gmail.com>
2021-04-26 22:53:45 +0900

    Make a symbolic link for the input annotation data dictionary in the CWAS workspace

--- cwas/configuration.py
+++ cwas/configuration.py
@@ -1,4 +1,5 @@
 import argparse
+import os
 from pathlib import Path
 
 import cwas.utils.log as log
@@ -31,6 +32,7 @@ class Configuration(Runnable):
     @staticmethod
     def _print_args(args: argparse.Namespace):
         log.print_arg('CWAS workspace', args.work_dir)
+        log.print_arg('Your annotation data dictionary', args.data_dir)
 
     @staticmethod
     def _check_args_validity(args: argparse.Namespace):
@@ -43,10 +45,20 @@ class Configuration(Runnable):
                     f"Non-directory file: '{args.work_dir}'")
 
     def run(self):
+        work_dir = getattr(self, 'work_dir')
+        data_dir = getattr(self, 'data_dir')
+        data_dir_symlink = work_dir / 'annotation-data'
         try:
-            self.args.work_dir.mkdir(parents=True, exist_ok=True)
+            log.print_progress(f'Create CWAS workspace "{work_dir}"')
+            work_dir.mkdir(parents=True, exist_ok=True)
+            log.print_progress(f'Create a symlink "{data_dir}" for your data '
+                               f'directory')
+            os.symlink(data_dir, data_dir_symlink, target_is_directory=True)
         except NotADirectoryError:
             log.print_err('The input CWAS workspace path is invalid.')
             raise
+        except FileExistsError:
+            log.print_warn(f'"{data_dir_symlink}" already exists so skip '
+                           f'making symbolic link for your data dictionary.')
 
         log.print_log('Notice', 'Not implemented yet.')
