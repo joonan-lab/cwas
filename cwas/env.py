f7aa910650f8042260f742c928f8a9bcf83f7a69
Minwoo Jeong <mwjeong.sci@gmail.com>
2021-07-05 22:42:49 +0900

    Create 'Singleton' and 'Env' classes

--- cwas/env.py
+++ cwas/env.py
@@ -3,22 +3,34 @@ Manage CWAS environment variables
 """
 
 from pathlib import Path
+
 import dotenv
-
-_ENV_PATH = Path(__file__).parent.resolve() / '.env'
-
-
-def init_env():
-    _ENV_PATH.touch()
+import yaml
 
 
-def env_exists() -> bool:
-    return _ENV_PATH.exists()
+# Ref: https://python-patterns.guide/gang-of-four/singleton/
+class Singleton(object):
+    _instance = None
+
+    def __new__(cls, *args, **kwargs):
+        if Singleton._instance is None:
+            cls._instance = super().__new__(cls)
+        return cls._instance
 
 
-def set_env(env_key: str, env_value: str):
-    dotenv.set_key(_ENV_PATH, env_key, env_value)
+class Env(Singleton):
+    def __init__(self):
+        self.path = Path(__file__).parent.resolve() / '.env'
+        if not self.path.exists():
+            self.path.touch()
+        self.env = dotenv.dotenv_values(dotenv_path=self.path)
 
+    def get_env(self, env_key):
+        return self.env.get(env_key)
 
-def load_env() -> dict:
-    return dotenv.dotenv_values(dotenv_path=_ENV_PATH)
+    def set_env(self, env_key, env_value):
+        self.env[env_key] = env_value
+
+    def save(self):
+        with self.path.open('w') as env_f:
+            yaml.safe_dump(self.env, env_f)
