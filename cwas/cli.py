21b8bebe5729b9327ed592a156cc4bbacdf7b0ae
Minwoo Jeong <mwjeong.sci@gmail.com>
2021-07-05 23:25:04 +0900

    Use Env class for CWAS environment variables

--- cwas/cli.py
+++ cwas/cli.py
@@ -3,7 +3,7 @@ Command line tool for Category-wide association study (CWAS)
 """
 import sys
 
-import cwas.env as env
+from cwas.env import Env
 from cwas.factory import get_runnable
 from cwas.utils.log import print_log
 
@@ -12,14 +12,12 @@ def main():
     # TODO: Add conditional statements or exception handling. This is very
     #  error-prone.
     print_log('LOG', 'Category-Wide Association Study (CWAS)')
-    if not env.env_exists():
-        env.init_env()
-    cwas_env = env.load_env()
+    cwas_env = Env()
     cwas_step_name = sys.argv[1]
     cwas_args = sys.argv[2:]
     cwas_obj = get_runnable(cwas_step_name)
     print_log('LOG', f'Current step: {cwas_obj.__name__}')
     cwas_inst = cwas_obj.get_instance(cwas_args)
-    if cwas_env:
-        setattr(cwas_inst, 'env', cwas_env)
+    setattr(cwas_inst, 'env', cwas_env)
     cwas_inst.run()
+    cwas_env.save()
