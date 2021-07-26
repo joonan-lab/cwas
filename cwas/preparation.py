6b8b91d465abce921d0e4cacc765c5e232da5436
Minwoo Jeong <mwjeong.sci@gmail.com>
2021-07-26 21:10:58 +0900

    Initialize 'Preparation' class

---
+++ cwas/preparation.py
@@ -0,0 +1,24 @@
+import argparse
+
+from cwas.runnable import Runnable
+from cwas.utils.log import print_log
+
+
+class Preparation(Runnable):
+    def __init__(self, args: argparse.Namespace):
+        super().__init__(args)
+
+    @staticmethod
+    def _create_arg_parser() -> argparse.ArgumentParser:
+        pass
+
+    @staticmethod
+    def _print_args(args: argparse.Namespace):
+        pass
+
+    @staticmethod
+    def _check_args_validity(args: argparse.Namespace):
+        pass
+
+    def run(self):
+        print_log('Notice', 'Not implemented yet.')
