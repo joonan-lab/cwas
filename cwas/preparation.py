22d1db7ca4eef86ff93fc5d94b0b46e2ce413dc4
Minwoo Jeong <mwjeong.sci@gmail.com>
2021-08-01 21:52:21 +0900

    Implement `run` and `_prepare_annotation` method
    Implement preparation logic for CWAS annotation step

--- cwas/preparation.py
+++ cwas/preparation.py
@@ -1,7 +1,12 @@
 import argparse
-import multiprocessing as mp
-from cwas.runnable import Runnable
+from multiprocessing import cpu_count
+from pathlib import Path
+
+import yaml
+
 import cwas.utils.log as log
+from cwas.core.preparation.annotation import merge_bed_files
+from cwas.runnable import Runnable
 
 
 class Preparation(Runnable):
@@ -30,10 +35,36 @@ class Preparation(Runnable):
     @staticmethod
     def _check_args_validity(args: argparse.Namespace):
         min_num_proc = 1
-        max_num_proc = mp.cpu_count()
+        max_num_proc = cpu_count()
         if args.num_proc < min_num_proc or args.num_proc > max_num_proc:
             raise ValueError(f'Wrong No. processes "{args.num_proc}" '
                              f'(range: {min_num_proc} ~ {max_num_proc})')
 
     def run(self):
-        log.print_log('Notice', 'Not implemented yet.')
+        self._prepare_annotation()
+
+    def _prepare_annotation(self):
+        log.print_progress(
+            'Data preprocessing to prepare CWAS annotation step')
+        cwas_env = getattr(self, 'env')
+        workspace = Path(cwas_env.get_env('CWAS_WORKSPACE'))
+        annot_data_dir = Path(cwas_env.get_env('ANNOTATION_DATA'))
+        bed_key_list_path = workspace / cwas_env.get_env('ANNOTATION_BED_KEY')
+
+        with bed_key_list_path.open() as bed_key_list_file:
+            bed_key_list = yaml.safe_load(bed_key_list_file)
+
+        bed_file_and_keys = []
+        for bed_filename, bed_key in bed_key_list.items():
+            bed_file_path = annot_data_dir / bed_filename
+            bed_file_and_keys.append((bed_file_path, bed_key))
+
+        log.print_progress(
+            'Merge all of your annotation BED files into one BED file')
+        num_proc = getattr(self, 'num_proc')
+        force_overwrite = getattr(self, 'force_overwrite')
+        merge_bed_path = workspace / 'merged_annotation.bed'
+        merge_bed_files(merge_bed_path, bed_file_and_keys,
+                        num_proc, force_overwrite)
+
+        cwas_env.set_env('MERGED_BED', merge_bed_path)
