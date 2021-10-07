c4a55ecda59297065ef87c2a25c139ee72ad4caa
Minwoo Jeong <mwjeong.sci@gmail.com>
2021-10-07 23:44:35 +0900

    Update tests for `cwas/annotation.py`

--- tests/test_annotation.py
+++ tests/test_annotation.py
@@ -31,41 +31,62 @@ def test_run_annotation_with_empty_env():
         inst.run()
 
 
-def test_default_args():
-    inst = AnnotationWithoutRun.get_instance([])
+def test_parse_args():
+    vcf_path = "test.vcf"
+    cpu = random.choice(range(1, cpu_count() + 1))
+    args = ["-i", vcf_path, "-p", str(cpu), "--per_chrom"]
+    inst = AnnotationWithoutRun.get_instance(args)
+    assert getattr(inst, "vcf_path") == vcf_path
+    assert getattr(inst, "num_proc") == cpu
+    assert getattr(inst, "per_chrom") == 1
+
+
+def test_parse_args_without_num_proc():
+    vcf_path = "test.vcf"
+    args = ["-i", vcf_path, "--per_chrom"]
+    inst = AnnotationWithoutRun.get_instance(args)
+    assert getattr(inst, "vcf_path") == vcf_path
+    assert getattr(inst, "num_proc") == 1
+    assert getattr(inst, "per_chrom") == 1
+
+
+def test_parse_args_required_only():
+    vcf_path = "test.vcf"
+    args = ["-i", vcf_path]
+    inst = AnnotationWithoutRun.get_instance(args)
+    assert getattr(inst, "vcf_path") == vcf_path
     assert getattr(inst, "num_proc") == 1
     assert getattr(inst, "per_chrom") == 0
 
 
-def test_parse_args():
-    cpu = random.choice(range(1, cpu_count() + 1))
-    args = ["-p", str(cpu), "--per_chrom"]
-    inst = AnnotationWithoutRun.get_instance(args)
-    assert getattr(inst, "num_proc") == cpu
-    assert getattr(inst, "per_chrom") == 1
-
-    args = ["--per_chrom"]
-    inst = AnnotationWithoutRun.get_instance(args)
-    assert getattr(inst, "num_proc") == 1
-    assert getattr(inst, "per_chrom") == 1
-
-
-def test_parse_args_value_error():
-    # '-p [int]' argument cannot be used without '--per_chrom' argument
-    cpu = random.choice(range(1, cpu_count() + 1))
-    args = ["-p", str(cpu)]
+def test_parse_args_without_required_arg():
+    args = []
     with pytest.raises(ValueError):
         AnnotationWithoutRun.get_instance(args)
-    
+
+
+def test_parse_args_invalid_num_proc():
+    vcf_path = "test.vcf"
+    base_args = ["-i", vcf_path, "--per_chrom"]
+
     cpu = cpu_count() + 1
-    args = ["-p", str(cpu)]
+    args = base_args + ["-p", str(cpu)]
     with pytest.raises(ValueError):
         AnnotationWithoutRun.get_instance(args)
 
-    args = ["-p", "0"]
+    args = base_args + ["-p", "0"]
     with pytest.raises(ValueError):
         AnnotationWithoutRun.get_instance(args)
 
-    args = ["-p", "-1"]
+    args = base_args + ["-p", "-1"]
+    with pytest.raises(ValueError):
+        AnnotationWithoutRun.get_instance(args)
+
+
+def test_parse_args_num_proc_without_per_chrom():
+    # '-p [int]' argument cannot be used without '--per_chrom' argument
+    vcf_path = "test.vcf"
+    cpu = random.choice(range(1, cpu_count() + 1))
+    args = ["-i", vcf_path, "-p", str(cpu)]
     with pytest.raises(ValueError):
         AnnotationWithoutRun.get_instance(args)
