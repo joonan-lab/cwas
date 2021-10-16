5f9b06abed78092715fd3c42fc5c26eccfc5af35
Minwoo Jeong <mwjeong.sci@gmail.com>
2021-10-16 10:26:19 +0900

    Implement argument parser for `Annotation` class
    * Update tests for this class
    * Implement methods for parsing arguments

--- cwas/annotation.py
+++ cwas/annotation.py
@@ -1,7 +1,16 @@
+"""
+CWAS Annotation Step
+
+This step annotate user's VCF file using annotation data specified 
+in the CWAS configuration step. This step mainly uses 
+Variant Effect Predictor (VEP) to annotate user's VCF file.
+"""
 import argparse
+from pathlib import Path
 
 from cwas.runnable import Runnable
-from cwas.utils.log import print_log
+from cwas.utils.error import check_is_file, check_num_proc
+from cwas.utils.log import print_arg, print_log
 
 
 class Annotation(Runnable):
@@ -10,15 +19,38 @@ class Annotation(Runnable):
 
     @staticmethod
     def _create_arg_parser() -> argparse.ArgumentParser:
-        pass
+        parser = argparse.ArgumentParser(
+            description="Arguments of CWAS annotation step",
+            formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+        )
+        parser.add_argument(
+            "-v",
+            "--vcf_file",
+            dest="vcf_path",
+            required=True,
+            type=Path,
+            help="Target VCF file",
+        )
+        parser.add_argument(
+            "-p",
+            "--num_process",
+            dest="num_proc",
+            required=False,
+            type=int,
+            help="Number of worker processes to use",
+            default=1,
+        )
+        return parser
 
     @staticmethod
     def _print_args(args: argparse.Namespace):
-        pass
+        print_arg("Target VCF file", args.vcf_path)
+        print_arg("Number of worker processes", args.num_proc)
 
     @staticmethod
     def _check_args_validity(args: argparse.Namespace):
-        pass
+        check_is_file(args.vcf_path)
+        check_num_proc(args.num_proc)
 
     def run(self):
-        print_log('Notice', 'Not implemented yet.')
+        print_log("Notice", "Not implemented yet.")
--- tests/test_annotation.py
+++ tests/test_annotation.py
@@ -5,11 +5,11 @@ import random
 from multiprocessing import cpu_count
 
 import pytest
-from cwas.env import Env
 from cwas.annotation import Annotation
+from cwas.env import Env
 
 
-class AnnotationWithoutRun(Annotation):
+class AnnotationMock(Annotation):
     """Mocking the Annotation class"""
 
     def run(self):
@@ -17,76 +17,88 @@ class AnnotationWithoutRun(Annotation):
 
 
 @pytest.fixture(scope="module")
-def env_mock(cwas_workspace):
+def vcf_path(cwas_workspace):
+    return cwas_workspace / "test_target.vcf"
+
+
+@pytest.fixture(scope="module", autouse=True)
+def create_vcf_file(vcf_path):
+    vcf_header = (
+        "#CHROM",
+        "POS",
+        "ID",
+        "REF",
+        "ALT",
+        "QUAL",
+        "FILTER",
+        "INFO",
+    )
+    # TODO: Add VCF entries (variants)
+    with vcf_path.open("w") as vcf_file:
+        print(*vcf_header, sep="\t", file=vcf_file)
+
+
+@pytest.fixture(scope="module")
+def required_args(vcf_path):
+    return ["-v", str(vcf_path)]
+
+
+@pytest.fixture(scope="function")
+def set_env(cwas_workspace, annotation_dir):
     env = Env()
+    env_path = cwas_workspace / ".cwas_config"
+    env.set_path(env_path)
     env.set_env("CWAS_WORKSPACE", cwas_workspace)
     env.set_env("VEP", "VEP")
-    return env
+    env.set_env("ANNOTATION_DATA", annotation_dir)
+    env.set_env("ANNOTATION_BW_KEY", annotation_dir / "bw_key.yaml")
+    env.set_env("MERGED_BED", cwas_workspace / "merged.bed.gz")
+    env.set_env("MERGED_BED_INDEX", cwas_workspace / "merged.bed.gz.tbi")
+    env.save()
 
 
-def test_run_annotation_with_empty_env():
-    inst = Annotation.get_instance([])
-    setattr(inst, "env", Env())
+def test_run_annotation_with_empty_env(required_args):
+    inst = AnnotationMock.get_instance(required_args)
+    assert inst.get_env("CWAS_WORKSPACE") is None
+    assert inst.get_env("VEP") is None
+    assert inst.get_env("ANNOTATION_DATA") is None
+    assert inst.get_env("ANNOTATION_BW_KEY") is None
+    assert inst.get_env("MERGED_BED") is None
+    assert inst.get_env("MERGED_BED_INDEX") is None
     with pytest.raises(RuntimeError):
         inst.run()
 
 
-def test_parse_args():
-    vcf_path = "test.vcf"
+def test_parse_args(required_args, vcf_path)    :
     cpu = random.choice(range(1, cpu_count() + 1))
-    args = ["-i", vcf_path, "-p", str(cpu), "--per_chrom"]
-    inst = AnnotationWithoutRun.get_instance(args)
+    args = required_args + ["-p", str(cpu)]
+    inst = AnnotationMock.get_instance(args)
     assert getattr(inst, "vcf_path") == vcf_path
     assert getattr(inst, "num_proc") == cpu
-    assert getattr(inst, "per_chrom") == 1
 
 
-def test_parse_args_without_num_proc():
-    vcf_path = "test.vcf"
-    args = ["-i", vcf_path, "--per_chrom"]
-    inst = AnnotationWithoutRun.get_instance(args)
+def test_parse_args_required_arg_only(required_args, vcf_path):
+    inst = AnnotationMock.get_instance(required_args)
     assert getattr(inst, "vcf_path") == vcf_path
     assert getattr(inst, "num_proc") == 1
-    assert getattr(inst, "per_chrom") == 1
-
-
-def test_parse_args_required_only():
-    vcf_path = "test.vcf"
-    args = ["-i", vcf_path]
-    inst = AnnotationWithoutRun.get_instance(args)
-    assert getattr(inst, "vcf_path") == vcf_path
-    assert getattr(inst, "num_proc") == 1
-    assert getattr(inst, "per_chrom") == 0
 
 
 def test_parse_args_without_required_arg():
     args = []
-    with pytest.raises(ValueError):
-        AnnotationWithoutRun.get_instance(args)
+    with pytest.raises(SystemExit):
+        AnnotationMock.get_instance(args)
 
 
-def test_parse_args_invalid_num_proc():
-    vcf_path = "test.vcf"
-    base_args = ["-i", vcf_path, "--per_chrom"]
-
+def test_parse_args_invalid_num_proc(required_args):
     cpu = cpu_count() + 1
-    args = base_args + ["-p", str(cpu)]
+    args = required_args + ["-p", str(cpu)]
     with pytest.raises(ValueError):
-        AnnotationWithoutRun.get_instance(args)
+        AnnotationMock.get_instance(args)
 
-    args = base_args + ["-p", "0"]
+    args = required_args + ["-p", "0"]
     with pytest.raises(ValueError):
-        AnnotationWithoutRun.get_instance(args)
+        AnnotationMock.get_instance(args)
 
-    args = base_args + ["-p", "-1"]
+    args = required_args + ["-p", "-1"]
     with pytest.raises(ValueError):
-        AnnotationWithoutRun.get_instance(args)
-
-
-def test_parse_args_num_proc_without_per_chrom():
-    # '-p [int]' argument cannot be used without '--per_chrom' argument
-    vcf_path = "test.vcf"
-    cpu = random.choice(range(1, cpu_count() + 1))
-    args = ["-i", vcf_path, "-p", str(cpu)]
-    with pytest.raises(ValueError):
-        AnnotationWithoutRun.get_instance(args)
+        AnnotationMock.get_instance(args)
