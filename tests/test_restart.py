6b5704052f0fd358e9807e2fdf46dfd7dac06591
Minwoo Jeong <mwjeong.sci@gmail.com>
2021-10-26 22:03:44 +0900

    Update tests for the `Start` step
    * Update tests/test_start.py
    * Create tests/test_restart.py

---
+++ tests/test_restart.py
@@ -0,0 +1,69 @@
+"""
+Tests the situation when we call CWAS Start again
+"""
+from pathlib import Path
+
+import pytest
+from cwas.start import Start
+
+
+@pytest.fixture(scope="module")
+def args(cwas_workspace: Path) -> list:
+    return ["-w", cwas_workspace]
+
+
+@pytest.fixture(scope="module")
+def config() -> dict:
+    test_config = {"TEST": "HELLO"}
+    return test_config
+
+
+@pytest.fixture(scope="module")
+def env() -> dict:
+    test_env = {"FAKE_ENV": "HI"}
+    return test_env
+
+
+@pytest.fixture(scope="module")
+def create_config_early(cwas_workspace: Path, config: dict):
+    cwas_config_path = cwas_workspace / "configuration.txt"
+
+    with cwas_config_path.open("w") as config_file:
+        for config_key, config_value in config.items():
+            print(f"{config_key}={config_value}", file=config_file)
+
+
+@pytest.fixture(scope="module")
+def create_env_early(env: dict):
+    cwas_env_path = Path.home() / ".cwas_env"
+
+    with cwas_env_path.open("w") as env_file:
+        for env_key, env_value in env.items():
+            print(f"{env_key}={env_value}", file=env_file)
+
+
+@pytest.fixture(scope="module", autouse=True)
+def run_start(args: list, create_config_early, create_env_early):
+    inst = Start.get_instance(args)
+    inst.run()
+
+
+def test_cwas_config(cwas_workspace: Path, config: dict):
+    cwas_config_path = cwas_workspace / "configuration.txt"
+    actual_config = get_config_from_file(cwas_config_path)
+    assert actual_config.items() >= config.items()
+
+
+def test_cwas_env(env: dict):
+    cwas_env_path = Path.home() / ".cwas_env"
+    actual_env = get_config_from_file(cwas_env_path)
+    assert actual_env.items() >= env.items()
+
+
+def get_config_from_file(config_path: Path) -> dict:
+    config_from_file = {}
+    with config_path.open() as config_file:
+        for line in config_file:
+            k, v = line.strip.split("=")
+            config_from_file[k] = v
+    return config_from_file
--- tests/test_start.py
+++ tests/test_start.py
@@ -1,24 +1,52 @@
 """
-Tests of the 'Start' class
+Tests of the 'Start' step
 """
+import os
+from pathlib import Path
+
 import pytest
 from cwas.start import Start
 
 
 @pytest.fixture(scope="module")
-def args(cwas_workspace):
+def args(cwas_workspace: Path) -> list:
     return ["-w", cwas_workspace]
 
 
-def test_start_step(args, cwas_workspace):
+@pytest.fixture(scope="module", autouse=True)
+def run_start(args: list):
     inst = Start.get_instance(args)
     inst.run()
-    check_initial_file_exist(cwas_workspace)
 
 
-def check_initial_file_exist(cwas_workspace):
-    cwas_env_file_path = cwas_workspace / ".env"
-    cwas_config_file_path = cwas_workspace / "configuration.txt"
-    assert cwas_workspace.isdir()
-    assert cwas_env_file_path.isfile()
-    assert cwas_config_file_path.isfile()
+def test_initial_file_exist(cwas_workspace: Path):
+    cwas_env_path = Path.home() / ".cwas_env"
+    cwas_config_path = cwas_workspace / "configuration.txt"
+    assert cwas_env_path.is_file()
+    assert cwas_workspace.is_dir()
+    assert cwas_config_path.is_file()
+
+
+def test_os_environ(cwas_workspace: Path):
+    assert os.getenv("CWAS_WORKSPACE") == str(cwas_workspace)
+
+
+def test_config_keys(cwas_workspace: Path):
+    config_key_set = set()
+    cwas_config_path = cwas_workspace / "configuration.txt"
+    with cwas_config_path.open() as config_file:
+        for line in config_file:
+            config_key, _ = line.strip().split("=")
+            config_key_set.add(config_key)
+
+    expected_key_set = {
+        "VEP",
+        "ANNOTATION_DATA",
+        "GENE_MATRIX",
+        "ANNOTATION_BED_KEY",
+        "ANNOTATION_VW_KEY",
+        "ANNOTATION_BW_CUTOFF",
+        "CATEGORY_DOMAIN",
+        "REDUNDANT_CATEGORY",
+    }
+    assert config_key_set == expected_key_set
