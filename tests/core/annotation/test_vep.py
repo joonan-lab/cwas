0efbaf884c178c5d2f116715172f4fd33ceeff5b
Minwoo Jeong <jeongmwj@gmail.com>
2021-11-27 00:23:29 +0900

    Add tests for setting input VCF path to the VEP instance

--- tests/core/annotation/test_vep.py
+++ tests/core/annotation/test_vep.py
@@ -1,4 +1,5 @@
 import shutil
+from pathlib import Path
 
 import pytest
 from cwas.core.annotation.vep import VEP
@@ -7,6 +8,14 @@ from cwas.core.annotation.vep import VEP
 @pytest.fixture(scope="module")
 def installed_vep() -> str:
     return shutil.which("vep")
+
+
+@pytest.fixture(scope="module")
+def input_vcf_path(cwas_workspace) -> Path:
+    tmp_vcf_path = cwas_workspace / "test_vep.vcf"
+    tmp_vcf_path.touch()
+    yield tmp_vcf_path
+    tmp_vcf_path.unlink()
 
 
 def test_init_vep(installed_vep):
@@ -28,3 +37,22 @@ def test_init_vep_with_no_arg():
 def test_init_vep_with_none():
     with pytest.raises(ValueError):
         _ = VEP(None)
+
+
+def test_set_input_vcf(installed_vep, input_vcf_path):
+    vep_inst = VEP(installed_vep)
+    vep_inst.set_input_vcf(input_vcf_path)
+    assert f"-i {input_vcf_path}" in vep_inst.get_cmd()
+
+
+def test_set_invalid_input_vcf(installed_vep, cwas_workspace):
+    vep_inst = VEP(installed_vep)
+    invalid_vcf_path = cwas_workspace / "test_not_exists.vcf"
+    with pytest.raises(FileNotFoundError):
+        vep_inst.set_input_vcf(invalid_vcf_path)
+
+
+def test_set_input_vcf_with_none():
+    vep_inst = VEP(installed_vep)
+    with pytest.raises(ValueError):
+        vep_inst.set_input_vcf(None)
