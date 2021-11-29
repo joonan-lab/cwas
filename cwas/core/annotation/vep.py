fbd828b1e6500f1df2977756929f1bf6173d2dfa
Minwoo Jeong <jeongmwj@gmail.com>
2021-11-29 21:45:11 +0900

    Implement the `VEP` contructor

--- cwas/core/annotation/vep.py
+++ cwas/core/annotation/vep.py
@@ -1,2 +1,13 @@
+from cwas.utils.check import check_is_file
+
+
 class VEP:
-    pass
+    def __init__(self, vep: str) -> None:
+        self.vep = vep
+        self._check_vep_path()
+
+    def _check_vep_path(self):
+        check_is_file(self.vep)
+
+    def get_vep_path(self):
+        return self.vep
