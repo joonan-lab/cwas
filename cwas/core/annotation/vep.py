7889c5d634984bc5c86b8aa0f75fcdcb0ef18943
Minwoo Jeong <jeongmwj@gmail.com>
2021-11-29 21:58:44 +0900

    Implement `set_input_vcf` method

--- cwas/core/annotation/vep.py
+++ cwas/core/annotation/vep.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 from cwas.utils.check import check_is_file
 
 
@@ -5,9 +7,17 @@ class VEP:
     def __init__(self, vep: str) -> None:
         self.vep = vep
         self._check_vep_path()
+        self.input_vcf_path = None
+
+    def get_vep_path(self):
+        return self.vep
+
+    def set_input_vcf(self, input_vcf_path: Path):
+        self.input_vcf_path = input_vcf_path
+        self._check_input_vcf_path()
 
     def _check_vep_path(self):
         check_is_file(self.vep)
 
-    def get_vep_path(self):
-        return self.vep
+    def _check_input_vcf_path(self):
+        check_is_file(self.input_vcf_path)
