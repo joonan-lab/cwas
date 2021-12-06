3266b005bf6eea336dd8221a1ab0157b9ef0a13e
Minwoo Jeong <jeongmwj@gmail.com>
2021-12-06 22:28:22 +0900

    Remove `set_input_vcf`

--- cwas/core/annotation/vep.py
+++ cwas/core/annotation/vep.py
@@ -2,19 +2,15 @@ from cwas.utils.check import check_is_file
 
 
 class VEP:
-    def __init__(self, vep_path: str) -> None:
+    def __init__(self, vep_path: str, input_vcf_path: str) -> None:
         check_is_file(vep_path)
+        check_is_file(input_vcf_path)
         self.vep_path = vep_path
-        self.input_vcf_path = None
-        self.output_vcf_path = None
+        self.input_vcf_path = input_vcf_path
+        self.output_vcf_path = input_vcf_path.replace(".vcf", ".annotated.vcf")
 
     def get_vep_path(self) -> str:
         return self.vep_path
-
-    def set_input_vcf(self, input_vcf_path: str):
-        check_is_file(input_vcf_path)
-        self.input_vcf_path = input_vcf_path
-        self.output_vcf_path = input_vcf_path.replace(".vcf", ".annotated.vcf")
 
     def get_cmd(self) -> str:
         return " ".join([self.vep_path, "-i", str(self.input_vcf_path)])
