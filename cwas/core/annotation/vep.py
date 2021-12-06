85f5023a03f27cd5a7a3cff7cb97486280ac6e69
Minwoo Jeong <jeongmwj@gmail.com>
2021-12-06 21:52:06 +0900

    Inline the check methods

--- cwas/core/annotation/vep.py
+++ cwas/core/annotation/vep.py
@@ -3,22 +3,16 @@ from cwas.utils.check import check_is_file
 
 class VEP:
     def __init__(self, vep_path: str) -> None:
+        check_is_file(vep_path)
         self.vep_path = vep_path
-        self._check_vep_path()
         self.input_vcf_path = None
 
     def get_vep_path(self) -> str:
         return self.vep_path
 
     def set_input_vcf(self, input_vcf_path: str):
+        check_is_file(input_vcf_path)
         self.input_vcf_path = input_vcf_path
-        self._check_input_vcf_path()
 
     def get_cmd(self) -> str:
         return " ".join([self.vep_path, "-i", str(self.input_vcf_path)])
-
-    def _check_vep_path(self):
-        check_is_file(self.vep_path)
-
-    def _check_input_vcf_path(self):
-        check_is_file(self.input_vcf_path)
