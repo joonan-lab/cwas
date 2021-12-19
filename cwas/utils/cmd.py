3ba57c5184b025bf81f840dc2db827c8582f4cb3
Minwoo Jeong <jeongmwj@gmail.com>
2021-12-19 13:14:31 +0900

    Remove `execute` and `execute_bin`
    and replace them with the method of 'CmdExecutor'

--- cwas/utils/cmd.py
+++ cwas/utils/cmd.py
@@ -47,33 +47,6 @@ class CmdExecutor:
         return output.returncode
 
 
-def execute(args: list, raise_err: bool = True) -> subprocess.CompletedProcess:
-    cmd = " ".join(args)
-    log.print_log("CMD", cmd, True)
-
-    try:
-        output = subprocess.run(args, check=raise_err)
-        if output.returncode != 0:
-            msg = f"This CMD has failed with exit status {output.returncode}."
-            log.print_warn(msg)
-        return output
-    except subprocess.CalledProcessError:
-        log.print_err("Your command has failed.")
-        raise
-
-
-def execute_bin(
-    bin_name: str, args: list, raise_err: bool = True
-) -> subprocess.CompletedProcess:
-    bin_path = shutil.which(bin_name)
-
-    if bin_path is None:
-        raise FileNotFoundError(f'The binary "{bin_name}" cannot be found.')
-
-    cmd = [bin_path] + args
-    return execute(cmd, raise_err)
-
-
 def compress_bed_file(bed_file_path: Path) -> Path:
     """Compress the BED file using bgzip"""
     gz_path = Path(str(bed_file_path) + ".gz")
@@ -86,7 +59,7 @@ def compress_bed_file(bed_file_path: Path) -> Path:
         check_is_file(bed_file_path)
 
         try:
-            execute_bin("bgzip", [str(bed_file_path)])
+            CmdExecutor("bgzip", [str(bed_file_path)]).execute_raising_err()
         except subprocess.CalledProcessError:
             log.print_err(
                 f'Failed to compress your BED file "{bed_file_path}".'
@@ -111,7 +84,7 @@ def index_bed_file(comp_bed_path: Path) -> Path:
         check_is_file(comp_bed_path)
 
         try:
-            execute_bin("tabix", [str(comp_bed_path)])
+            CmdExecutor("tabix", [str(comp_bed_path)]).execute_raising_err()
         except subprocess.CalledProcessError:
             log.print_err(
                 f'Failed to index your compressed BED file "{comp_bed_path}".'
