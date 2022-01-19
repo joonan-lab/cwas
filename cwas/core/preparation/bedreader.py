865480d49848c537134100e4ad6aa375341b27d8
Minwoo Jeong <jeongmwj@gmail.com>
2022-01-19 19:29:26 +0900

    Handle the error which raises when the contig is invalid or does not exist

--- cwas/core/preparation/bedreader.py
+++ cwas/core/preparation/bedreader.py
@@ -3,6 +3,7 @@ A container class that contains iterator protocol to read a BED file
 """
 from __future__ import annotations
 import pysam
+from cwas.utils.log import print_warn
 
 
 class BedReader:
@@ -19,12 +20,18 @@ class BedReader:
             )
 
     def __iter__(self):
-        with pysam.TabixFile(str(self.bed_path)) as bed_file:
-            for fields in bed_file.fetch(
-                self.contig, self.start, self.stop, parser=pysam.asTuple()
-            ):
-                contig, start, stop, *others = tuple(fields)
-                yield (contig, int(start), int(stop), *others)
+        try:
+            with pysam.TabixFile(str(self.bed_path)) as bed_file:
+                for fields in bed_file.fetch(
+                    self.contig, self.start, self.stop, parser=pysam.asTuple()
+                ):
+                    contig, start, stop, *others = tuple(fields)
+                    yield (contig, int(start), int(stop), *others)
+        except ValueError:
+            print_warn(
+                f"The contig '{self.contig}' does not exist "
+                f"in the bed file '{self.bed_path}'."
+            )
 
     def set_contig(self, contig: str):
         self.contig = contig
