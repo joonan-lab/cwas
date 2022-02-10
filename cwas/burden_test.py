30d23cf5a82ba15226e238f925217d814998a075
Minwoo Jeong <jeongmwj@gmail.com>
2022-02-10 22:54:55 +0900

    Add logic to adjust the categorization result

--- cwas/burden_test.py
+++ cwas/burden_test.py
@@ -4,6 +4,7 @@ from pathlib import Path
 
 import pandas as pd
 
+from cwas.core.common import cmp_two_arr
 from cwas.runnable import Runnable
 from cwas.utils.check import check_is_file
 from cwas.utils.log import print_arg, print_log, print_progress
@@ -12,9 +13,9 @@ from cwas.utils.log import print_arg, print_log, print_progress
 class BurdenTest(Runnable):
     def __init__(self, args: argparse.Namespace):
         super().__init__(args)
-        self._categorization_result = None
         self._sample_info = None
         self._adj_factor = None
+        self._categorization_result = None
 
     @staticmethod
     def _create_arg_parser() -> argparse.ArgumentParser:
@@ -53,15 +54,6 @@ class BurdenTest(Runnable):
             check_is_file(args.adj_factor_path)
 
     @property
-    def categorization_result(self) -> pd.DataFrame:
-        if self._categorization_result is None:
-            print_progress("Load the categorization result")
-            self._categorization_result = pd.read_table(
-                self.get_env("CATEGORIZATION_RESULT"), index_col="SAMPLE"
-            )
-        return self._categorization_result
-
-    @property
     def sample_info(self) -> pd.DataFrame:
         if self._sample_info is None:
             self._sample_info = pd.read_table(
@@ -79,6 +71,34 @@ class BurdenTest(Runnable):
             )
         return self._adj_factor
 
+    @property
+    def categorization_result(self) -> pd.DataFrame:
+        if self._categorization_result is None:
+            print_progress("Load the categorization result")
+            self._categorization_result = pd.read_table(
+                self.get_env("CATEGORIZATION_RESULT"), index_col="SAMPLE"
+            )
+            if self.adj_factor is not None:
+                self._adjust_categorization_result()
+        return self._categorization_result
+
+    def _adjust_categorization_result(self):
+        if not _contain_same_index(
+            self._categorization_result, self.adj_factor
+        ):
+            raise ValueError(
+                "The sample IDs from the adjustment factor list are "
+                "not the same with the sample IDs "
+                "from the categorization result."
+            )
+        adj_factors = [
+            self.adj_factor.to_dict()["AdjustFactor"][sample_id]
+            for sample_id in self._categorization_result.index.values
+        ]
+        self._categorization_result = self._categorization_result.multiply(
+            adj_factors, axis="index"
+        )
+
     def run(self):
         print_log("Notice", "Not implemented yet.")
 
@@ -87,3 +107,7 @@ class BurdenTest(Runnable):
         raise RuntimeError(
             "This method cannot be called via the instance of BurdenTest."
         )
+
+
+def _contain_same_index(table1: pd.DataFrame, table2: pd.DataFrame) -> bool:
+    return cmp_two_arr(table1.index.values, table2.index.values)
