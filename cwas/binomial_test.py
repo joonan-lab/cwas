86fa6836576f7cb1385f1b167162c008b58a4fcb
Minwoo Jeong <jeongmwj@gmail.com>
2022-02-11 20:20:45 +0900

    Move `get_case_variant_cnt` into the class
    as 'case_variant_cnt' property

--- cwas/binomial_test.py
+++ cwas/binomial_test.py
@@ -1,4 +1,4 @@
-from typing import Tuple
+import argparse
 
 import numpy as np
 import pandas as pd
@@ -9,6 +9,23 @@ from cwas.core.burden_test.binomial import binom_one_tail, binom_two_tail
 
 
 class BinomialTest(BurdenTest):
+    def __init__(self, args: argparse.Namespace):
+        super().__init__(args)
+        self._case_variant_cnt = None
+
+    @property
+    def case_variant_cnt(self) -> np.ndarray:
+        if self._case_variant_cnt is None:
+            phenotypes = np.vectorize(
+                lambda sample_id: self.sample_info.to_dict()["PHENOTYPE"][
+                    sample_id
+                ]
+            )(self.categorization_result.index.values)
+            self._case_variant_cnt = self.categorization_result.values[
+                phenotypes == "case", :
+            ].sum(axis=0)
+        return self._case_variant_cnt
+
     @property
     def binom_p(self) -> float:
         phenotypes = self.sample_info["PHENOTYPE"].values
@@ -22,10 +39,10 @@ class BinomialTest(BurdenTest):
         sample_types = np.vectorize(
             lambda sample_id: sample_info_dict["PHENOTYPE"][sample_id]
         )(sample_ids)
-        case_dnv_cnt = get_case_variant_cnt(cwas_cat_vals, sample_types)
         ctrl_dnv_cnt = get_ctrl_variant_cnt(cwas_cat_vals, sample_types)
         dnv_cnt_arr = np.concatenate(
-            [case_dnv_cnt[:, np.newaxis], ctrl_dnv_cnt[:, np.newaxis]], axis=1
+            [self.case_variant_cnt[:, np.newaxis], ctrl_dnv_cnt[:, np.newaxis]],
+            axis=1,
         )
 
         # Make a DataFrame for the results of binomial tests
@@ -35,27 +52,23 @@ class BinomialTest(BurdenTest):
             columns=["Case_DNV_Count", "Ctrl_DNV_Count"],
         )
         burden_df.index.name = "Category"
-        burden_df["Relative_Risk"] = case_dnv_cnt / ctrl_dnv_cnt
+        burden_df["Relative_Risk"] = self.case_variant_cnt / ctrl_dnv_cnt
         burden_df["P"] = np.vectorize(binom_two_tail)(
-            case_dnv_cnt.round(), ctrl_dnv_cnt.round(), self.binom_p
+            self.case_variant_cnt.round(), ctrl_dnv_cnt.round(), self.binom_p
         )
 
         # Following metrics is for getting number of effective tests
         # Add a pseudo-count in order to avoid p-values of one
         burden_df["P_1side"] = np.vectorize(binom_one_tail)(
-            case_dnv_cnt.round() + 1, ctrl_dnv_cnt.round() + 1, self.binom_p
+            self.case_variant_cnt.round() + 1,
+            ctrl_dnv_cnt.round() + 1,
+            self.binom_p,
         )
         burden_df["Z_1side"] = norm.ppf(
             1 - burden_df["P_1side"].values
         )  # P-value -> Z-score
 
         self._result = burden_df
-
-
-def get_case_variant_cnt(
-    categorization_result_values: np.ndarray, sample_types: np.ndarray
-) -> np.ndarray:
-    return categorization_result_values[sample_types == "case", :].sum(axis=0)
 
 
 def get_ctrl_variant_cnt(
