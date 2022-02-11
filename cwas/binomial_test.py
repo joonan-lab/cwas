29d059ddd5349f59baf61061cc9d0368abfe0272
Minwoo Jeong <jeongmwj@gmail.com>
2022-02-11 19:22:48 +0900

    Implement `run_burden_test` with the old version

--- cwas/binomial_test.py
+++ cwas/binomial_test.py
@@ -1,6 +1,67 @@
+from typing import Tuple
+
+import numpy as np
+import pandas as pd
+from scipy.stats import binom_test, norm
+
 from cwas.burden_test import BurdenTest
 
 
 class BinomialTest(BurdenTest):
     def run_burden_test(self):
-        raise NotImplementedError("Binomial test has not been implemented yet.")
+        # Count the number of de novo variants (DNV) for cases and controls
+        cwas_cat_vals = self.categorization_result.values
+        sample_info_dict = self.sample_info.to_dict()
+        sample_ids = self.categorization_result.index.values
+        sample_types = np.vectorize(
+            lambda sample_id: sample_info_dict["PHENOTYPE"][sample_id]
+        )(sample_ids)
+        case_dnv_cnt, ctrl_dnv_cnt = cnt_case_ctrl_dnv(
+            cwas_cat_vals, sample_types
+        )
+        dnv_cnt_arr = np.concatenate(
+            [case_dnv_cnt[:, np.newaxis], ctrl_dnv_cnt[:, np.newaxis]], axis=1
+        )
+
+        # Make a DataFrame for the results of binomial tests
+        burden_df = pd.DataFrame(
+            dnv_cnt_arr,
+            index=self.categorization_result.columns.values,
+            columns=["Case_DNV_Count", "Ctrl_DNV_Count"],
+        )
+        burden_df.index.name = "Category"
+        burden_df["Relative_Risk"] = case_dnv_cnt / ctrl_dnv_cnt
+
+        # Binomial tests
+        def binom_two_tail(n1: int, n2: int):
+            return binom_test(x=n1, n=n1 + n2, p=0.5, alternative="two-sided")
+
+        def binom_one_tail(n1: int, n2: int):
+            return binom_test(x=n1, n=n1 + n2, p=0.5, alternative="greater")
+
+        burden_df["P"] = np.vectorize(binom_two_tail)(
+            case_dnv_cnt.round(), ctrl_dnv_cnt.round()
+        )
+
+        # Following metrics is for getting number of effective tests
+        # Add a pseudo-count in order to avoid p-values of one
+        burden_df["P_1side"] = np.vectorize(binom_one_tail)(
+            case_dnv_cnt.round() + 1, ctrl_dnv_cnt.round() + 1
+        )
+        burden_df["Z_1side"] = norm.ppf(
+            1 - burden_df["P_1side"].values
+        )  # P-value -> Z-score
+
+        self._result = burden_df
+
+
+def cnt_case_ctrl_dnv(
+    sample_cat_vals: np.ndarray, sample_types: np.ndarray
+) -> Tuple[float, float]:
+    """ Count the number of the de novo variants for each phenotype, case and control.
+    """
+    are_case = sample_types == "case"
+    case_dnv_cnt = sample_cat_vals[are_case, :].sum(axis=0)
+    ctrl_dnv_cnt = sample_cat_vals[~are_case, :].sum(axis=0)
+
+    return case_dnv_cnt, ctrl_dnv_cnt
