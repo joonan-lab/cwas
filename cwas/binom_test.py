02f384e243c052375a1eda206e136837547c5235
Minwoo Jeong <jeongmwj@gmail.com>
2022-02-11 17:25:13 +0900

    Rewrite `BinomTest` and `PermutTest`

--- cwas/binom_test.py
+++ cwas/binom_test.py
@@ -1,20 +1,6 @@
-import argparse
-
 from cwas.burden_test import BurdenTest
 
 
 class BinomTest(BurdenTest):
-    @staticmethod
-    def _create_arg_parser() -> argparse.ArgumentParser:
-        pass
-
-    @staticmethod
-    def _print_args(args: argparse.Namespace):
-        pass
-
-    @staticmethod
-    def _check_args_validity(args: argparse.Namespace):
-        pass
-
     def run_burden_test(self):
-        pass
+        raise NotImplementedError("Binomial test has not been implemented yet.")
--- cwas/permut_test.py
+++ cwas/permut_test.py
@@ -1,20 +1,8 @@
-import argparse
-
 from cwas.burden_test import BurdenTest
 
 
 class PermutTest(BurdenTest):
-    @staticmethod
-    def _create_arg_parser() -> argparse.ArgumentParser:
-        pass
-
-    @staticmethod
-    def _print_args(args: argparse.Namespace):
-        pass
-
-    @staticmethod
-    def _check_args_validity(args: argparse.Namespace):
-        pass
-
     def run_burden_test(self):
-        pass
+        raise NotImplementedError(
+            "Permutation test has not been implemented yet."
+        )
