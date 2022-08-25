63b98a717f9d27e107020487196b75bff874d846
Minwoo Jeong <jeongmwj@gmail.com>
2022-08-25 22:31:04 +0900

    Remove `get_runnable` funciton

--- cwas/factory.py
+++ cwas/factory.py
@@ -40,13 +40,3 @@ def make_class_name(module_name: str) -> str:
     class_name = module_name.title()
     class_name = "".join(class_name.split("_"))
     return class_name
-
-
-def get_runnable(module_name: str) -> Runnable:
-    try:
-        module = importlib.import_module(f"cwas.{module_name}")
-        runnable = getattr(module, make_class_name(module_name))
-    except (ImportError, AttributeError):
-        raise ValueError(f'CWAS does not support "{module_name}".')
-
-    return runnable
--- tests/test_factory.py
+++ tests/test_factory.py
@@ -11,19 +11,6 @@ def test_make_class_name():
     assert cwas.factory.make_class_name("burden_test") == "BurdenTest"
 
 
-def test_get_runnable():
-    runnable = cwas.factory.get_runnable("categorization")
-    assert runnable.__name__ == "Categorization"
-    assert hasattr(runnable, "run")
-
-    runnable = cwas.factory.get_runnable("burden_test")
-    assert runnable.__name__ == "BurdenTest"
-    assert hasattr(runnable, "run")
-
-    with pytest.raises(ValueError):
-        cwas.factory.get_runnable("not_runnable")
-
-
 def test_create_factory():
     factory_inst = cwas.factory.create("categorization")
     assert isinstance(factory_inst.argparser(), argparse.ArgumentParser)
