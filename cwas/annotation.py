bab63ee2064737a08d78012d34183b0b95ef7064
koh2ng0 <kohingyeong@gmail.com>
2023-06-16 23:06:20 +0900

    Add compressing and indexing for input VCF file

--- cwas/annotation.py
+++ cwas/annotation.py
@@ -7,6 +7,7 @@ Variant Effect Predictor (VEP) to annotate user's VCF file.
 """
 import argparse
 from pathlib import Path
+import os
 
 import yaml
 
@@ -25,6 +26,7 @@ import dotenv
 class Annotation(Runnable):
     def __init__(self, args: argparse.Namespace):
         super().__init__(args)
+        self._vcf_path = None
 
     @staticmethod
     def _create_arg_parser() -> argparse.ArgumentParser:
@@ -75,7 +77,15 @@ class Annotation(Runnable):
 
     @property
     def vcf_path(self):
-        return self.args.vcf_path.resolve()
+        if self._vcf_path is None:
+            if self.args.n_cores > 1:
+                vcf_gz = compress_using_bgzip(self.args.vcf_path)
+                index_using_tabix(vcf_gz)
+                self._vcf_path = vcf_gz
+            else:
+                self._vcf_path = self.args.vcf_path.resolve()
+        
+        return self._vcf_path
     
     @property
     def n_cores(self):
@@ -125,9 +135,9 @@ class Annotation(Runnable):
         self.annotate_using_vep()
         self.process_vep_vcf()
         self.annotate_using_bed()
-        self.update_env()
+        self.update_env()        
 
-    def annotate_using_vep(self):
+    def annotate_using_vep(self):      
         print_progress("Annotation via VEP")
         if (
             Path(self.vep_output_vcf_path).is_file()
