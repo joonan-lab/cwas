4ed4a26ba3ccbb7a8205ea4af27d880b09ec8c0c
koh2ng0 <kohingyeong@gmail.com>
2023-06-26 18:56:23 +0900

    Add argument for plot in binomial test

--- cwas/argparser.py
+++ cwas/argparser.py
@@ -222,6 +222,41 @@ def binomial_test() -> argparse.ArgumentParser:
         action="store_true",
         help="Use the number of samples with variants in each category for burden test instead of the number of variants",
     )
+    result.add_argument(
+        '-t',
+        '--tag',
+        dest='tag',
+        default=None,
+        type=str,
+        required=False,
+        help="Query keyword of category for highlighting points on the volcano plot"
+    )
+    result.add_argument(
+        "-ms",
+        "--marker_size",
+        dest="marker_size",
+        required=False,
+        type=float,
+        default=15,
+        help="Maker size of the volcano plot resulted from the binomial test (unit: pt)",
+    )
+    result.add_argument(
+        "-fs",
+        "--font_size",
+        dest='font_size',
+        required=False,
+        type=float,
+        default=15,
+        help="Font size of the volcano plot resulted from the binomial test (unit: pt)",
+    )
+    result.add_argument(
+        '-ps',
+        '--plot_size',
+        required=False,
+        type=float,
+        default=7,
+        help="Plot size of the volcano plot resulted from the binomial test, width and height are the same (unit: inch)"
+    )
     return result
 
 
