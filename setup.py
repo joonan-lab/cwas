acd69a9ad648bad042fb000805c808bde8102c89
YJ <37772661+randrover@users.noreply.github.com>
2023-08-08 23:50:16 +0900

    Update package versions
    Update package versions

--- setup.py
+++ setup.py
@@ -31,6 +31,6 @@ setuptools.setup(
             'cwas = cwas.__main__:main'
         ]
     },
-    install_requires=["numpy", "pandas", "scipy", "tqdm", "glmnet", "igraph", "matplotlib", "rpy2", "seaborn", "parmap",
-                      "polars", "pyvcf", "scikit-learn", "pysam", "pytabix", "pyyaml", "python-dotenv", "pytest"]
+    install_requires=["numpy", "pandas", "scipy", "tqdm", "igraph", "matplotlib", "rpy2", "seaborn", "parmap",
+                      "polars", "pyvcf", "scikit-learn", "pysam", "pytabix", "pyyaml", "python-dotenv", "pytest", "glmnet"]
 )
