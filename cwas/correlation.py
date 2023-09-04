fb7b0bb46f12a6413ecf7740f4514828dbc07672
Yujin Kim <37772661+randrover@users.noreply.github.com>
2023-09-04 22:06:56 +0900

    Update correlation code
    Update correlation code

--- cwas/correlation.py
+++ cwas/correlation.py
@@ -9,6 +9,7 @@ import parmap
 from cwas.core.common import chunk_list
 from tqdm import tqdm
 import re
+import polars as pl
 
 import pandas as pd
 import numpy as np
@@ -26,18 +27,12 @@ from cwas.utils.check import check_num_proc
 from cwas.utils.check import check_is_file
 from cwas.utils.check import check_is_dir
 
-class Categorization(Runnable):
+class Correlation(Runnable):
     def __init__(self, args: argparse.Namespace):
         super().__init__(args)
-        self._annotated_vcf = None
-        self._gene_matrix = None
-        self._category_domain = None
-        self._annotated_vcf_groupby_sample = None
-        self._sample_ids = None
-        self._result = None
+        self._categorization_result = None
         self._correlation_matrix = None
         self._intersection_matrix = None
-        self._shm = None
 
     @staticmethod
     def _print_args(args: argparse.Namespace):
@@ -45,13 +40,13 @@ class Categorization(Runnable):
             "No. worker processes for the categorization",
             f"{args.num_proc: ,d}",
         )
-        log.print_arg("Annotated VCF file", args.input_path)
+        log.print_arg("Categorized file", args.cat_path)
         log.print_arg("Genereate a correlation matrix and an intersection matrix", (False if args.generate_matrix is None else True))
 
     @staticmethod
     def _check_args_validity(args: argparse.Namespace):
         check_num_proc(args.num_proc)
-        check_is_file(args.input_path)
+        check_is_file(args.cat_path)
         check_is_dir(args.output_dir_path)
 
     @property
@@ -59,8 +54,18 @@ class Categorization(Runnable):
         return self.args.num_proc
 
     @property
-    def input_path(self):
-        return self.args.input_path.resolve()
+    def cat_path(self) -> Path:
+        return self.args.cat_path.resolve()
+
+    @property
+    def categorization_result(self) -> pd.DataFrame:
+        if self._categorization_result is None:
+            log.print_progress("Load the categorization result")
+            self._categorization_result = pl.read_csv(
+                self.cat_path, separator="\t", dtypes={"SAMPLE": str}
+            )
+            self._categorization_result = self._categorization_result.to_pandas().set_index("SAMPLE")
+        return self._categorization_result
 
     @property
     def output_dir_path(self):
@@ -71,24 +76,8 @@ class Categorization(Runnable):
         return self.args.generate_matrix
 
     @property
-    def mis_info_key(self) -> str:
-        return self.get_env("VEP_MIS_INFO_KEY")
-
-    @property
-    def mis_thres(self) -> float:
-        return float(self.get_env("VEP_MIS_THRES"))
-
-    @property
-    def result_path(self) -> Path:
-        f_name = re.sub(r'annotated\.vcf\.gz|annotated\.vcf', 'categorization_result.txt.gz', self.input_path.name)
-        return Path(
-            f"{self.output_dir_path}/" + 
-            f"{f_name}"
-        )
-
-    @property
     def matrix_path(self) -> Path:
-        f_name = re.sub(r'annotated\.vcf\.gz|annotated\.vcf', 'correlation_matrix.pkl', self.input_path.name)
+        f_name = re.sub(r'annotated\.vcf\.gz|annotated\.vcf', 'correlation_matrix.pkl', self.cat_path.name)
         return Path(
             f"{self.output_dir_path}/" +
             f"{f_name}"
@@ -96,136 +85,17 @@ class Categorization(Runnable):
 
     @property
     def intersection_matrix_path(self) -> Path:
-        f_name = re.sub(r'annotated\.vcf\.gz|annotated\.vcf', 'intersection_matrix.pkl', self.input_path.name)
+        f_name = re.sub(r'annotated\.vcf\.gz|annotated\.vcf', 'intersection_matrix.pkl', self.cat_path.name)
         return Path(
             f"{self.output_dir_path}/" +
             f"{f_name}"
         )
 
-    @property
-    def annotated_vcf(self) -> pd.DataFrame:
-        if self._annotated_vcf is None:
-            log.print_progress("Parse the annotated VCF")
-            self._annotated_vcf = parse_annotated_vcf(
-                Path(self.input_path)
-            )
-        return self._annotated_vcf
-
-    @property
-    def gene_matrix(self) -> dict:
-        if self._gene_matrix is None:
-            self._gene_matrix = parse_gene_matrix(
-                Path(self.get_env("GENE_MATRIX"))
-            )
-        return self._gene_matrix
-
-    @property
-    def category_domain(self) -> dict:
-        if self._category_domain is None:
-            with Path(self.get_env("CATEGORY_DOMAIN")).open(
-                "r"
-            ) as category_domain_file:
-                self._category_domain = yaml.safe_load(category_domain_file)
-        return self._category_domain
-
-    @property
-    def categorizer(self) -> Categorizer:
-        categorizer = Categorizer(self.category_domain, self.gene_matrix, self.mis_info_key, self.mis_thres)
-        return categorizer
-
-    @property
-    def annotated_vcf_groupby_sample(self):
-        if self._annotated_vcf_groupby_sample is None:
-            self._annotated_vcf_groupby_sample = self.annotated_vcf.groupby(
-                "SAMPLE"
-            )
-        return self._annotated_vcf_groupby_sample
-
-    @property 
-    def sample_ids(self) -> list:
-        if self._sample_ids is None:
-            self._sample_ids = list(self.annotated_vcf_groupby_sample.groups)
-        return self._sample_ids
-
-    @property
-    def annotated_vcf_split_by_sample(self) -> list:
-        return [
-            self.annotated_vcf_groupby_sample.get_group(sample_id)
-            for sample_id in self.sample_ids
-        ]
-
-    @property
-    def redundant_categories(self) -> set:
-        redundant_category_table = pd.read_table(
-            self.get_env("REDUNDANT_CATEGORY")
-        )
-        return reduce(
-            lambda x, y: x.union(y),
-            [
-                self._get_redundant_categories_from_row(row)
-                for _, row in redundant_category_table.iterrows()
-            ],
-            set(),
-        )
-
-    def _get_redundant_categories_from_row(self, row: pd.Series) -> list:
-        annotation_term_lists = {}
-        for k, v in row.items():
-            if v == "*":
-                annotation_term_lists[k] = self._category_domain[k]
-            else:
-                annotation_term_lists[k] = [v]
-
-        return {
-            "_".join(combination)
-            for combination in product(
-                annotation_term_lists["variant_type"],
-                annotation_term_lists["gene_set"],
-                annotation_term_lists["functional_score"],
-                annotation_term_lists["gencode"],
-                annotation_term_lists["functional_annotation"],
-            )
-        }
-
     def run(self):
-        self.categorize_vcf()
-        self.remove_redundant_category()
         self.generate_correlation_matrix()
         self.save_result()
         self.update_env()
         log.print_progress("Done")
-
-    def categorize_vcf(self):
-        results_each_sample = (
-            self.categorize_vcf_for_each_sample()
-            if self.num_proc == 1
-            else self.categorize_vcf_for_each_sample_with_mp()
-        )
-        log.print_progress("Organize the results")
-        self._result = pd.DataFrame(results_each_sample).fillna(0)
-        self._result = self._result.astype(int)
-        self._result["SAMPLE"] = self.sample_ids
-        self._result.set_index("SAMPLE", inplace=True)
-
-    def remove_redundant_category(self):
-        log.print_progress("Remove redundant categories from the result")
-        self._result.drop(
-            self.redundant_categories,
-            axis="columns",
-            inplace=True,
-            errors="ignore",
-        )
-        log.print_progress(
-            f"{len(self._result.columns):,d} categories have remained."
-        )
-
-    def categorize_vcf_for_each_sample(self):
-        result = []
-        for i, sample_vcf in enumerate(self.annotated_vcf_split_by_sample, 1):
-            if i % 100 == 0:
-                log.print_progress(f"Categorize variants of {i} samples")
-            result.append(self.categorizer.categorize_variant(sample_vcf))
-        return result
 
     def categorize_vcf_for_each_sample_with_mp(self):
         sample_vcfs = self.annotated_vcf_split_by_sample
