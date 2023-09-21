64a0c4a736a62dc27809863f172a340eef355de9
Yujin Kim <37772661+randrover@users.noreply.github.com>
2023-09-21 21:32:44 +0900

    Update effective_num_test.py

--- cwas/effective_num_test.py
+++ cwas/effective_num_test.py
@@ -22,7 +22,6 @@ class EffectiveNumTest(Runnable):
         self._category_set = None
         self._num_eig = None
         self.eff_num_test_value = None
-        self._replace_term = None
         self._binom_p = None
         self._sample_info = None
         
@@ -175,23 +174,10 @@ class EffectiveNumTest(Runnable):
         return self.args.tag
 
     @property
-    def replace_term(self) -> str:
-        if self._replace_term is None:
-            if 'correlation_matrix.zarr' in str(self.input_path):
-                self._replace_term = r'\.correlation_matrix\.zarr\.gz|\.correlation_matrix\.zarr'
-            elif 'intersection_matrix.zarr' in str(self.input_path):
-                self._replace_term = r'\.intersection_matrix\.zarr\.gz|\.intersection_matrix\.zarr'
-            else:
-                self._replace_term = r'\.txt\.gz|\.txt'
-        return self._replace_term
-
-    @property
     def neg_lap_path(self) -> Path:
-        if self.tag is None:
-            save_name = '.neg_lap.zarr'
-        else:
-            save_name = '.'.join(['.neg_lap', self.tag, 'zarr'])
-        f_name = re.sub(self.replace_term, save_name, self.input_path.name)
+        replace_term = r'\.intersection_matrix|\.correlation_matrix'
+        save_name = '.neg_lap.zarr' if self.tag is None else f'.neg_lap.{self.tag}.zarr'
+        f_name = re.sub(replace_term, save_name, self.input_path.name)
         return Path(
             f"{self.output_dir_path}/" +
             f"{f_name}"
@@ -199,11 +185,9 @@ class EffectiveNumTest(Runnable):
 
     @property
     def eig_val_path(self) -> Path:
-        if self.tag is None:
-            save_name = '.eig_vals.zarr'
-        else:
-            save_name = '.'.join(['.eig_vals', self.tag, 'zarr'])
-        f_name = re.sub(self.replace_term, save_name, self.input_path.name)
+        replace_term = r'\.intersection_matrix|\.correlation_matrix'
+        save_name = '.eig_vals.zarr' if self.tag is None else f'.eig_vals.{self.tag}.zarr'
+        f_name = re.sub(replace_term, save_name, self.input_path.name)
         return Path(
             f"{self.output_dir_path}/" +
             f"{f_name}"
@@ -211,11 +195,9 @@ class EffectiveNumTest(Runnable):
 
     @property
     def eig_vec_path(self) -> Path:
-        if self.tag is None:
-            save_name = '.eig_vecs.zarr'
-        else:
-            save_name = '.'.join(['.eig_vecs', self.tag, 'zarr'])
-        f_name = re.sub(self.replace_term, save_name, self.input_path.name)
+        replace_term = r'\.intersection_matrix|\.correlation_matrix'
+        save_name = '.eig_vecs.zarr' if self.tag is None else f'.eig_vecs.{self.tag}.zarr'
+        f_name = re.sub(replace_term, save_name, self.input_path.name)
         return Path(
             f"{self.output_dir_path}/" +
             f"{f_name}"
